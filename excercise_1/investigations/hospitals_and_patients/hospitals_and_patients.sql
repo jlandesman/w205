ceaecd03f20b4d320e6275bb3dd132c5775b5f8b
jlandesman <jlandesman@gmail.com>
2016-04-13 22:09:35 -0400

    ex2

--- excercise_1/investigations/hospitals_and_patients/hospitals_and_patients.sql
+++
@@ -1,7 +0,0 @@
-drop table if exists survey_scores;
-
-create table survey_scores as
-select hospital_name, state_abbrev, cast(overall_achievement as int) as overall_score 
-from survey_clean
-order by overall_score desc
-limit 20;
